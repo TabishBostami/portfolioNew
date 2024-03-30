c0e1f3244ed03b7d4f288bf9b6b8f4162cd6f930
Tabish Bostami <tabish.bostami042000@gmail.com>
2024-03-30 11:50:24 +0530

    Add files via upload

---
+++ portfolio/game.js
@@ -0,0 +1,11 @@
+const boxes =Array.from(document.getElementsByClassName('box'));
+console.log(boxes);
+
+const drawBoard =() => {
+    boxes.forEach((box,index) =>{
+        let styleString ='';
+        if(index <3){
+            styleString += 'border-bottom: 3px solid rgb(68, 1, 68)';
+        }
+    })
+}
